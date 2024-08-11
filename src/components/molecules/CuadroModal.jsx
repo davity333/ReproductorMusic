f999dd6ff9366eac0171e6b770d8e19d16dd7012
david guzman <reyguz421@gmail.com>
2024-08-10 18:15:37 -0600

    playlistt

--- src/components/molecules/CuadroModal.jsx
+++ src/components/molecules/CuadroModal.jsx
@@ -3,11 +3,11 @@ function CuadroModal({onClick}) {
         <>
         
         <div className="fixed inset-0 bg-black bg-opacity-0 backdrop-blur-sm flex justify-center items-center">
-            <div className="bg-[#94a6b9] h-[50%] w-[40%] rounded">  
-            <p className="text-2xl text-center pt-11">Ingresa el nombre de la PlayList</p>
+            <div className="bg-[#b1bac5] h-[50%] w-[40%] rounded">  
+            <p className="text-2xl text-center pt-11">Nombre de la PlayList</p>
 
             <div className="flex justify-center items-center">
-                <input type="text" className="bg-slate-500"/>
+                <input type="text" className="bg-[#dbd1d1] border-black"/>
             <button className="pt-28" onClick={onClick}>Salir</button>
             </div>
             </div>
